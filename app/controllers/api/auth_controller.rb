00fa8820e71e886c37a584119c4aa216049adac3
YMGYM <good_day1234@naver.com>
2022-02-23 23:56:36 +0900

    [FIX]티어 오류 해결

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -1,7 +1,7 @@
 class Api::AuthController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:test, :sign_up, :sign_in] # csrf 토큰 스킵
   # before_action :jwt_authenticate_request!, only[:test]
-
+;
   # def test
   #   # 그냥 테스트 메소드
   #   @dataJson = { :message => "Token 인증 완료", :user => @current_user }
@@ -10,9 +10,9 @@ class Api::AuthController < ApplicationController
 
   def sign_up
     @user = User.new(user_params)
-
+    debugger
     if @user.save
-      render json: @user # TODO: AMS serialize
+      render json: payload(@user)
     else
       render json: { errors: ['Not Authenticated'] }, status: :unauthorized # TODO: 에러 코드 맞는지 확인
     end
@@ -21,9 +21,9 @@ class Api::AuthController < ApplicationController
   def sign_in
     json_params = JSON.parse(request.body.read)
 
-    user = User.find_for_database_authentication(email: user_params[:email])
-    if user.valid_password?(user_params[:password])
-      render json: payload(user)
+    @user = User.find_for_database_authentication(email: user_params[:email])
+    if @user.valid_password?(user_params[:password])
+      render json: payload(@user)
     else
       render json: { errors: ['Invalid Username/Password']}, status: :unauthorized
     end
@@ -32,8 +32,8 @@ class Api::AuthController < ApplicationController
   private
   def payload(user)
     # TODO : 그냥 30일 기준으로 판단하는 단순한 코드. Refresh Token 을 사용하지 않기 때문에 이를 사용하는 코드로 변경이 필요하다.
-    @token = JWT.encode({ user_id: user.id, exp: 30.days.from_now.to_i}, ENV["SECRET_KEY_BASE"])
-    @tree = { :"JWT token" => @token, :userInfo => { id: user.id, email: user.email } }
+    @token = JWT.encode({ user_id: @user.id, exp: 30.days.from_now.to_i}, ENV["SECRET_KEY_BASE"])
+    @tree = { :"JWT token" => @token, :userInfo => { id: @user.id, email: @user.email } }
     return @tree
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :workouts
-  belongs_to :tier, class_name: "tier", foreign_key: "tier_id"
+  has_one :tier
 end
